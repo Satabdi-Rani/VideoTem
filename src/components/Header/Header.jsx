7cfd3b441760cf8cfbc3ab2c989466b113a09562
Satabdi Rani <32415291+Satabdi-Rani@users.noreply.github.com>
2023-08-31 15:15:02 +0600

    Changing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,44 @@ import {FiSearch} from "react-icons/fi";
 
 const Header = () => {
     return (
-        <div className='flex flex-row mt-6'>
+        <div className='grid grid-cols-3'>
+           <div className="grid-cols-[repeat(1,1fr))]">
+               <BiMenu size={30} className=''/>
+               <BsYoutube size={30} className=' fill-red-800'/>
+               <p className='font-bold'>YOUTUBE <sup>BD</sup> </p>
+            </div>
+            <div className="grid-cols-auto">
+                <div className='border-2 rounded-full'>
+                    <input className='border-r ' type="text" placeholder='Search' />
+                    <FiSearch className=' bg-slate-50' size={20}/>
+                </div>
+                <div className='rounded-full  bg-slate-50'>
+                <BsFillMicFill className='' size={20}/>
+                </div>
+            </div>
+            <div className='grid-cols-[repeat(1,1fr))]'>
+                    <ul>
+                        <li>
+                            <BiSolidVideoPlus />
+                        </li>
+                        <li>
+                           
+                        </li>
+                        <li></li>
+                    </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
+
+
+{/* <div className='flex flex-row mt-6'>
             <div className="burger_menu flex-initial w-64 flex flex-row">
                <BiMenu size={30} className='ml-6 '/>
                <BsYoutube size={30} className='ml-3 fill-red-800'/>
-               <p className='font-bold ml-2'>YOUTUBE</p>
+               <p className='font-bold ml-2'>YOUTUBE <sup>BD</sup> </p>
             </div>
             <div className="search_option flex flex-row ml-72 ">
                 <div className='flex flex-row py-2 border-2 rounded-full'>
@@ -34,8 +67,4 @@ const Header = () => {
                 </div>
                 
             </div>
-        </div>
-    );
-};
-
-export default Header;
+        </div> */}
